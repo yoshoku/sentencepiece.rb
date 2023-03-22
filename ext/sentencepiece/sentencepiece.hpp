475e2ed03f3a526a041e0203f309acf9288f5aa8
yoshoku <yoshoku@outlook.com>
2023-03-22 21:24:17 +0900

    feat: add encode_as_pieces method to SentencePieceProcessor

--- ext/sentencepiece/sentencepiece.hpp
+++ ext/sentencepiece/sentencepiece.hpp
@@ -58,6 +58,7 @@ public:
     rb_define_method(rb_cSentencePieceProcessor, "load", RUBY_METHOD_FUNC(_sentencepiece_processor_load), 1);
     rb_define_method(rb_cSentencePieceProcessor, "encode", RUBY_METHOD_FUNC(_sentencepiece_processor_encode), -1);
     rb_define_method(rb_cSentencePieceProcessor, "encode_as_ids", RUBY_METHOD_FUNC(_sentencepiece_processor_encode_as_ids), 1);
+    rb_define_method(rb_cSentencePieceProcessor, "encode_as_pieces", RUBY_METHOD_FUNC(_sentencepiece_processor_encode_as_pieces), 1);
     rb_define_method(rb_cSentencePieceProcessor, "decode", RUBY_METHOD_FUNC(_sentencepiece_processor_decode), -1);
     rb_define_method(rb_cSentencePieceProcessor, "piece_size", RUBY_METHOD_FUNC(_sentencepiece_processor_piece_size), 0);
     rb_define_method(rb_cSentencePieceProcessor, "piece_to_id", RUBY_METHOD_FUNC(_sentencepiece_processor_piece_to_id), 1);
@@ -209,6 +210,23 @@ private:
     VALUE output = rb_ary_new();
     for (const int idx : ids) {
       rb_ary_push(output, INT2NUM(idx));
+    }
+
+    RB_GC_GUARD(text);
+    return output;
+  };
+
+  static VALUE _sentencepiece_processor_encode_as_pieces(VALUE self, VALUE text) {
+    if (!RB_TYPE_P(text, T_STRING)) {
+      rb_raise(rb_eArgError, "expected text to be a String");
+      return Qnil;
+    }
+
+    sentencepiece::SentencePieceProcessor* ptr = get_sentencepiece_processor(self);
+    const std::vector<std::string> pieces = ptr->EncodeAsPieces(StringValueCStr(text));
+    VALUE output = rb_ary_new();
+    for (const std::string& token : pieces) {
+      rb_ary_push(output, rb_utf8_str_new_cstr(token.c_str()));
     }
 
     RB_GC_GUARD(text);
