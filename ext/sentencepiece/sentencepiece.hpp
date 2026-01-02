eea8135eac7cc136d472765e97aab2764d12ce10
yoshoku <yoshoku@outlook.com>
2026-01-02 10:54:01 +0900

    docs: update copyright year

--- ext/sentencepiece/sentencepiece.hpp
+++ ext/sentencepiece/sentencepiece.hpp
@@ -1,7 +1,7 @@
 /**
  * sentencepiece.rb provides Ruby bindings for the SentencePiece.
  *
- * Copyright (c) 2023-2025 Atsushi Tatsuma
+ * Copyright (c) 2023-2026 Atsushi Tatsuma
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
